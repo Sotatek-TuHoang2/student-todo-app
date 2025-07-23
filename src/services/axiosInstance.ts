77bce08b87740ff12ad9cec7616d52a771d74adc
Sotatek-TuHoang2 <tu.hoang2@sotatek.com>
2025-07-23 17:24:26 +0700

    Create Axios Config

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://localhost:3001',
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => config,
+    (error) => {
+        console.error('Loi API:', error);
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
