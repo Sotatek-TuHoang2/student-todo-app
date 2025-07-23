d298957361641d476d27b71774b1eeb65b57bcdb
Sotatek-TuHoang2 <tu.hoang2@sotatek.com>
2025-07-23 17:11:30 +0700

    Init Base & Creat types

---
+++ src/types/Student.ts
@@ -0,0 +1,14 @@
+export interface Student {
+    id: string;
+    fullName: string;
+    email: string;
+    dob: string;
+    class: string;
+}
+
+export interface StudentFormData {
+    fullName: string;
+    email: string;
+    dob: string;
+    class: string;
+}
